64b845a5ff4b3b8c8f031f70cfc25d7ec2bb9123
zc <zhangchanget@126.com>
2021-09-04 15:19:47 +0800

    接口自动化代码完整版

--- API/test_case/model/test_model.py
+++ API/test_case/model/test_model.py
@@ -4,27 +4,31 @@
 # @Author  : zc
 # @File    : test_model.py
 
-
+import allure
 from API.test_case.base_testcase import BaseTestCase
 
-
+@allure.feature('执行以下测试用例：')
 class TestModel(BaseTestCase):
 
+    @allure.story('测试用例1：查询接口')
     def test_searchModel(self):
 
         result = self.model.searchModel()
         assert 2001 == result['code'],"与预想结果不对！"
 
+    @allure.story('测试用例1：添加接口')
     def test_addModel(self):
 
         result = self.model.addModel()
         assert 200 == result['code'],"与预想结果不对！"
 
+    @allure.story('测试用例1：删除接口')
     def test_deleteModel(self):
 
         result = self.model.deleteModel()
         assert 200 == result['code'],"与预想结果不对！"
 
+    @allure.story('测试用例1：更新接口')
     def test_updateModel(self):
 
         result = self.model.updateModel()
